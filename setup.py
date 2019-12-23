564f2e96b4548da0b1e2d429e20efc4e5c5e5bc9
Parker Erickson <parker.erickson30@gmail.com>
2019-12-23 16:26:02 -0600

    adding project description to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'pyTigerGraph',         # How you named your package folder (MyLib)
   packages = ['pyTigerGraph'],   # Chose the same as "name"
   version = '0.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library to connect to TigerGraph databases',   # Give a short description about your library
+  long_description=long_description,
   author = 'Parker Erickson',                   # Type in your name
   author_email = 'parker.erickson30@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/parkererickson/pyTigerGraph',   # Provide either the link to your github or to your website
@@ -12,8 +16,7 @@ setup(
   keywords = ['TigerGraph', 'Graph Database'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
-          'requests',
-      ],
+          'requests'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
