864f005d5b367c38348b0d1a087b919712344d1e
Parker Erickson <parker.erickson30@gmail.com>
2020-05-12 15:56:47 -0500

    Setup.py update for adding all REST endpoints to master:

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", encoding='utf-8') as fh:
 setup(
   name = 'pyTigerGraph',         # How you named your package folder (MyLib)
   packages = ['pyTigerGraph'],   # Chose the same as "name"
-  version = '0.0.4.7.6',      # Start with a small number and increase it with every change you make
+  version = '0.0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library to connect to TigerGraph databases',   # Give a short description about your library
   long_description=long_description,
   long_description_content_type='text/markdown',
-  author = 'Parker Erickson',                   # Type in your name
+  author = 'Parker Erickson / TigerGraph',                   # Type in your name
   author_email = 'parker.erickson30@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/parkererickson/pyTigerGraph',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/parkererickson/pyTigerGraph/archive/v_01.tar.gz',    # I explain this later on
@@ -20,7 +20,7 @@ setup(
           'validators',
           'requests'],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
