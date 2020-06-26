73ab742cfbafa4868908ebcbefefa49c79ac5072
Parker Erickson <parker.erickson30@gmail.com>
2020-06-25 21:37:48 -0500

    working without any error checking or hidden file creation

--- pyTigerGraph/gsql.py
+++ pyTigerGraph/gsql.py
@@ -3,31 +3,35 @@ import os
 import subprocess, yaml, re
 
 class gsql():
-    def __init__(self, connection, client_version, jarLocation="~/.gsql/", certNeeded=True, certLocation="~/.gsql/my-cert.txt"):
+    def __init__(self, connection, client_version, jarLocation="./", certNeeded=True, certLocation="./my-cert.txt"):
         self.connection = connection
+        self.jarLocation = jarLocation
+        self.certLocation = certLocation
+        self.url = connection.gsUrl[8:] # Getting URL with gsql port w/o https://
+        '''
         if (noJava):
             Exception("Install Java")
-        if (jarDoesntExist):
-            if (dirDoesntExist):
-                mkdir
-            jar_url = ('https://bintray.com/api/ui/download/tigergraphecosys/tgjars/' 
-                    + 'com/tigergraph/client/gsql_client/' + client_version 
-                    + '/gsql_client-' + client_version + '.jar')
-                    
-            urllib.request.urlretrieve(jar_url, jarLocation + 'gsql_client.jar') # TODO: Store this with the package?
+        '''
+        jar_url = ('https://bintray.com/api/ui/download/tigergraphecosys/tgjars/' 
+                + 'com/tigergraph/client/gsql_client/' + client_version 
+                + '/gsql_client-' + client_version + '.jar')
+                
+        urllib.request.urlretrieve(jar_url, jarLocation + 'gsql_client.jar') # TODO: Store this with the package?
         
         if(certNeeded): #HTTP/HTTPS
+            '''
             if (noOpenSSL):
                 Exception("No OpenSSL, provide own certificication")
-            if(certDoesntExist):
-                openssl s_client -connect connection.gsUrl < /dev/null 2> /dev/null | openssl x509 -text > my-cert.txt  # TODO: Python-native SSL?
-                ls -l my-cert.txt
+            '''
+            #if(certDoesntExist):
+            print(self.connection.gsUrl[8:])
+            os.system("openssl s_client -connect "+self.url+" < /dev/null 2> /dev/null | openssl x509 -text > "+self.certLocation) # TODO: Python-native SSL?
 
 
-    def gsql(query, options=None):
+    def gsql(self, query, options=None):
         cmd = ['java', '-DGSQL_CLIENT_VERSION=v2_6_0', '-jar', 'gsql_client.jar',
-        '-cacert', 'my-cert.txt', '-u', self.connection.username, '-p', self.connection.password, 
-        '-ip', self.connection.gsUrl]
+        '-cacert', self.certLocation, '-u', self.connection.username, '-p', self.connection.password, 
+        '-ip', self.url]
         if (not options):
             comp = subprocess.run(cmd + ["-g", self.connection.graphname] + [query], 
                                 stdout=subprocess.PIPE).stdout.decode()
@@ -43,10 +47,10 @@ class gsql():
         else:
             return json_object
         
-    def createSecret(alias=""):
+    def createSecret(self, alias=""):
         response = self.gsql("CREATE SECRET"+" "+alias)
         try:
-            secret = re.search('The secret\: (\w*)',comp.replace('\n',''))[1]
+            secret = re.search('The secret\: (\w*)',response.replace('\n',''))[1]
             return secret
         except:
             return None
