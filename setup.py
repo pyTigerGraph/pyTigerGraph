8cda2d61fd52ab7670c04233b698d319c79d8a08
Parker Erickson <parker.erickson30@gmail.com>
2020-07-20 20:40:50 -0500

    v 0.0.6.6 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding='utf-8') as fh:
 setup(
   name = 'pyTigerGraph',         # How you named your package folder (MyLib)
   packages = ['pyTigerGraph'],   # Chose the same as "name"
-  version = '0.0.6.5',      # Start with a small number and increase it with every change you make
+  version = '0.0.6.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library to connect to TigerGraph databases',   # Give a short description about your library
   long_description=long_description,
@@ -18,7 +18,8 @@ setup(
   keywords = ['TigerGraph', 'Graph Database'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
-          'requests'],
+          'requests',
+          'pandas'],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
