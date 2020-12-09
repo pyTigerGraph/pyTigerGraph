c42c4e8d48d3016abe21a77c6433fecad9877f1f
Zrouga Mohamed <zrouga.mohammed@gmail.com>
2020-12-09 14:25:11 +0100

    Added Circle & Unit Test ...

--- test/test_pyTigerGraph.py
+++ test/test_pyTigerGraph.py
@@ -1,3 +1,12 @@
 import pyTigerGraph as tg
 
-conn = tg.TigerGraphConnection(host="https://medzrouga.", graphname="MyGraph", username="tigergraph", password="tigergraph", apiToken="<api_token>")
+conn = tg.TigerGraphConnection(host="https://<BOX>", graphname="MyGraph", username="tigergraph",
+                               password="tigergraph", apiToken="<api_token>")
+
+class TestpyTigerGraph:
+
+    def testgsql(self):
+        assert True == conn.gsql("LS")
+
+    def testgetVer(self):
+        assert False == conn.getVer()
