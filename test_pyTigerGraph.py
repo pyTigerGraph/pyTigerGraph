aaec2da24e9d17e2b24fbf5830c4580e6d5ece7c
Zrouga Mohamed <zrouga.mohammed@gmail.com>
2020-12-11 13:49:34 +0100

    Test Case , Merge with Shreya Tests ...

--- test_pyTigerGraph.py
+++ test_pyTigerGraph.py
@@ -1,94 +1,72 @@
 import pyTigerGraph as tg
+import pytest
 
 conn = tg.TigerGraphConnection(host="https://medzrouga.i.tgcloud.io", graphname="MyGraph", username="tigergraph",password="tigergraph", apiToken="3mva82884g93sofo5s8tksrnoo7il9v5")
 
 class TestpyTigerGraph:
 
+    @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
     def testgsql(self):
         assert conn.gsql("LS")[0] == "---- Global vertices, edges, and all graphs"
+
+    @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
+    def test_get_statistics(self):
+        conn.gsql("drop all")
+        conn.gsql('''
+            CREATE VERTEX Test (PRIMARY_ID id STRING) WITH primary_id_as_attribute="true"
+        ''', options=[])
+        conn.gsql('''CREATE GRAPH TestGraph(Test)''', options=[])
+        conn.graphname = "TestGraph"
+        conn.apiToken = conn.getToken(conn.createSecret())[0]
+        val = conn.getStatistics()
+
+        # Assert
+        assert val == {}
 
     def testgetVer(self):
         assert "3.0.5" == conn.getVer()
 
-    def test_get_statistics(self):
-        conn.gsql('''
-            CREATE VERTEX Test (PRIMARY_ID id STRING) WITH primary_id_as_attribute="true"
-        ''', options=[])
-        conn.gsql('''CREATE GRAPH TestGraph(Test)''', options=[])
-        conn.graphname = "TestGraph"
-        # conn.apiToken = conn.getToken(conn.createSecret())
-        val = conn.getStatistics()
-        # Drops the graph created
-        conn.gsql('''
-            USE GRAPH TestGraph
-            DROP VERTEX Test
-            DROP GRAPH TestGraph
-        ''')
-        # Assert
-        assert val == {}
-
+    @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
     def test_run_interpreted_query(self):
-        conn.gsql('''
-            CREATE VERTEX Test (PRIMARY_ID id STRING) WITH primary_id_as_attribute="true"
-        ''', options=[])
-        conn.gsql('''CREATE GRAPH TestGraph(Test)''', options=[])
+
         conn.graphname = "TestGraph"
-        # conn.apiToken = conn.getToken(conn.createSecret())
+        conn.apiToken = conn.getToken(conn.createSecret())[0]
         val = conn.runInterpretedQuery('''
             INTERPRET QUERY () FOR GRAPH TestGraph {
                 Seed = {Test.*};
                 PRINT Seed;
             }
         ''')
-        # Drops the graph created
-        conn.gsql('''
-            USE GRAPH TestGraph
-            DROP VERTEX Test
-            DROP GRAPH TestGraph
-        ''')
+
         # Assert
         assert val == [{'Seed': []}]
 
+    @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
     def test_get_installed_queries(self):
-        conn.gsql('''
-            CREATE VERTEX Test (PRIMARY_ID id STRING) WITH primary_id_as_attribute="true"
-        ''', options=[])
-        conn.gsql('''CREATE GRAPH TestGraph(Test)''', options=[])
+
         conn.graphname = "TestGraph"
-        # conn.apiToken = conn.getToken(conn.createSecret())
+        conn.apiToken = conn.getToken(conn.createSecret())[0]
         val = conn.getInstalledQueries()
-        # Drops the graph created
-        conn.gsql('''
-            USE GRAPH TestGraph
-            DROP VERTEX Test
-            DROP GRAPH TestGraph
-        ''')
+
         # Assert
         assert val == {}
 
+    @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
     def test_run_installed_query(self):
-        conn.gsql('''
-            CREATE VERTEX Test (PRIMARY_ID id STRING) WITH primary_id_as_attribute="true"
-        ''', options=[])
-        conn.gsql('''CREATE GRAPH TestGraph(Test)''', options=[])
+
         conn.graphname = "TestGraph"
-        # conn.apiToken = conn.getToken(conn.createSecret())
+        conn.gsql("USE GRAPH TestGraph")
+        conn.apiToken = conn.getToken(conn.createSecret())[0]
+        conn.gsql('USE GRAPH TestGraph')
         conn.gsql('''
-            USE GRAPH TestGraph
             CREATE QUERY TestQuery() FOR GRAPH TestGraph {
                 Seed = {Test.*};
                 PRINT Seed;
             }
-            INSTALL QUERY TestQuery
         ''', options=[])
+        conn.gsql("INSTALL QUERY TestQuery")
         val = conn.runInstalledQuery("TestQuery")
-        # Drops the graph created
-        conn.gsql('''
-            USE GRAPH TestGraph
-            DROP QUERY TestQuery
-            DROP VERTEX Test
-            DROP GRAPH TestGraph
-        ''', options=[])
+        conn.gsql("drop all")
         # Assert
         assert val == [{'Seed': []}]
 
