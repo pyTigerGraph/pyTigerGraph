81e0e5b357bfbfa8b45f283b529fa058b615eb8c
Zrouga Mohamed <zrouga.mohammed@gmail.com>
2021-01-28 18:45:45 +0100

    updating the driver ...

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding='utf-8') as fh:
 setup(
   name = 'pyTigerGraphBeta',         # How you named your package folder (MyLib)
   packages = ['pyTigerGraphBeta'],   # Chose the same as "name"
-  version = '0.0.9.5',      # Start with a small number and increase it with every change you make
+  version = '0.0.9.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Library to connect to TigerGraph databases - Beta Realease',   # Give a short description about your library
   long_description=long_description,
