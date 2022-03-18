49ea5c500b24c38f74f786cd8448b3856415be16
Szilard Barany <szilard.barany@tigergraph.com>
2022-03-18 23:53:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,37 +4,35 @@ with open("README.md", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name='pyTigerGraph',  # How you named your package folder (MyLib)
-    packages=['pyTigerGraph'],  # Chose the same as "name"
-    version='0.0.9.9.2',  # Start with a small number and increase it with every change you make
+    name='pyTigerGraph',
+    packages=['pyTigerGraph'],
+    version='0.0.9.9.2',
     license='MIT',
-    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Library to connect to TigerGraph databases',
-    # Give a short description about your library
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Zrouga Mohamed / Jonathan Herke / Parker Erickson / Szilard Barany / TigerGraph',
-    # Type in your name
-    author_email='medzrouga@gmail.com',  # Type in your E-Mail
-    url='https://github.com/pyTigerGraph/pyTigerGraph',
-    # Provide either the link to your github or to your website
-    download_url='',  # I explain this later on
-    keywords=['TigerGraph', 'Graph Database'],  # Keywords that define your package best
-    install_requires=[  # I get to this in a second
+    author='TigerGraph Inc.',
+    author_email='support@tigergraph.com',
+    url='https://www.tigergraph.com/',
+    download_url='',
+    keywords=['TigerGraph', 'Graph Database'],
+    install_requires=[
         'pyTigerDriver',
         'validators',
         'requests',
         'pandas'],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Development Status :: 4 - Beta',  # 3 - Alpha, 4 - Beta or 5 - Production/Stable
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        # Specify which pyhton versions that you want to support
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
