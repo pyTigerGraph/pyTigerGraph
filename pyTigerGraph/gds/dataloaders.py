5411123f0d1f038bffd5aac282cdff4ca7ee6591
Bill Shi <bill.shi@tigergraph.com>
2022-04-05 16:33:05 -0700

    feat: add neighbor loader and tests

--- pyTigerGraph/gds/dataloaders.py
+++ pyTigerGraph/gds/dataloaders.py
@@ -114,7 +114,7 @@ class BaseLoader:
                     client_id=self.loader_id,
                     max_partition_fetch_bytes=KafkaMaxMsgSize,
                     fetch_max_bytes=KafkaMaxMsgSize,
-                    auto_offset_reset="earliest",
+                    auto_offset_reset="earliest"
                 )
                 self._kafka_admin = KafkaAdminClient(
                     bootstrap_servers=self.kafka_address_consumer,
@@ -309,7 +309,7 @@ class BaseLoader:
             tgraph.restppUrl + "/query/" + tgraph.graphname + "/" + query_name,
             data=_payload,
             headers=_headers,
-            resKey=None,
+            resKey=None
         )
         # Check status
         _stat_payload = {
@@ -710,3 +710,265 @@ class BaseLoader:
         # Parse data
         # Return data
         raise NotImplementedError
+
+
+class NeighborLoader(BaseLoader):
+    def __init__(
+        self,
+        graph: TigerGraphConnection,
+        v_in_feats: Union[list, dict] = None,
+        v_out_labels: Union[list, dict] = None,
+        v_extra_feats: Union[list, dict] = None,
+        batch_size: int = None,
+        num_batches: int = 1,
+        num_neighbors: int = 10,
+        num_hops: int = 2,
+        shuffle: bool = False,
+        filter_by: str = None,
+        output_format: str = "PyG",
+        add_self_loop: bool = False,
+        loader_id: str = None,
+        buffer_size: int = 4,
+        kafka_address: str = None,
+        kafka_max_msg_size: int = 104857600,
+        kafka_num_partitions: int = 1,
+        kafka_replica_factor: int = 1,
+        kafka_retention_ms: int = 60000,
+        kafka_auto_del_topic: bool = True,
+        kafka_address_consumer: str = None,
+        kafka_address_producer: str = None,
+        timeout: int = 300000,
+    ) -> None:
+        """A data loader that performs neighbor sampling as introduced in the
+        [Inductive Representation Learning on Large Graphs](https://arxiv.org/abs/1706.02216) paper.
+
+        Specifically, it first chooses `batch_size` number of vertices as seeds,
+        then picks `num_neighbors` number of neighbors of each seed at random,
+        then `num_neighbors` neighbors of each neighbor, and repeat for `num_hops`.
+        This generates one subgraph. As you loop through this data loader, every
+        vertex will at some point be chosen as a seed and you will get the subgraph
+        expanded from the seed. If you want to limit seeds to certain vertices, the boolean
+        attribute provided to `filter_by` will be used to indicate which vertices can be
+        included as seeds.
+
+        **Note**: For the first time you initialize the loader on a graph in TigerGraph,
+        the initialization might take a minute as it installs the corresponding
+        query to the database and optimizes it. However, the query installation only
+        needs to be done once, so it will take no time when you initialize the loader
+        on the same TG graph again.
+
+        There are two ways to use the data loader. See [here](https://github.com/TigerGraph-DevLabs/mlworkbench-docs/blob/main/tutorials/basics/2_dataloaders.ipynb)
+        for examples.
+
+        * First, it can be used as an iterable, which means you can loop through
+          it to get every batch of data. If you load all data at once (`num_batches=1`),
+          there will be only one batch (of all the data) in the iterator.
+        * Second, you can access the `data` property of the class directly. If there is
+          only one batch of data to load, it will give you the batch directly instead
+          of an iterator, which might make more sense in that case. If there are
+          multiple batches of data to load, it will return the loader itself.
+
+        Args:
+          What data to get:
+            graph (TigerGraphConnection): Connection to the TigerGraph database.
+            v_in_feats (list, optional): Vertex attributes to be used as input features.
+                Only numeric and boolean attributes are allowed. The type of an attrbiute
+                is automatically determined from the database schema. Defaults to None.
+            v_out_labels (list, optional): Vertex attributes to be used as labels for
+                prediction. Only numeric and boolean attributes are allowed. Defaults to None.
+            v_extra_feats (list, optional): Other attributes to get such as indicators of
+                train/test data. All types of attributes are allowed. Defaults to None.
+          How to get the data:
+            batch_size (int, optional):  Number of vertices as seeds in each batch.
+                Defaults to None.
+            num_batches (int, optional): Number of batches to split the vertices into as seeds.
+                Defaults to 1.
+            num_neighbors (int, optional): Number of neighbors to sample for each vertex.
+                Defaults to 10.
+            num_hops (int, optional): Number of hops to traverse when sampling neighbors.
+                Defaults to 2.
+            shuffle (bool, optional): Whether to shuffle the vertices before loading data.
+                Defaults to False.
+            filter_by (str, optional): A boolean attribute used to indicate which vertices
+                can be included as seeds. Defaults to None.
+          What is the output:
+            output_format (str, optional): Format of the output data of the loader. Only
+                "PyG", "DGL" and "dataframe" are supported. Defaults to "PyG".
+            add_self_loop (bool, optional): Whether to add self-loops to the graph. Defaults to False.
+          Low-level details of the loader:
+            loader_id (str, optional): An identifier of the loader which can be any string. It is
+                also used as the Kafka topic name. If `None`, a random string will be generated
+                for it. Defaults to None.
+            buffer_size (int, optional): Number of data batches to prefetch and store in memory. Defaults to 4.
+            kafka_address (str, optional): Address of the kafka broker. Defaults to None.
+            kafka_max_msg_size (int, optional): Maximum size of a Kafka message in bytes.
+                Defaults to 104857600.
+            kafka_num_partitions (int, optional): Number of partitions for the topic created by this loader.
+                Defaults to 1.
+            kafka_replica_factor (int, optional): Number of replications for the topic created by this
+                loader. Defaults to 1.
+            kafka_retention_ms (int, optional): Retention time for messages in the topic created by this
+                loader in milliseconds. Defaults to 60000.
+            kafka_address_consumer (str, optional): Address of the kafka broker that a consumer
+                should use. Defaults to be the same as `kafkaAddress`.
+            kafka_address_producer (str, optional): Address of the kafka broker that a producer
+                should use. Defaults to be the same as `kafkaAddress`.
+            timeout (int, optional): Timeout value for GSQL queries, in ms. Defaults to 300000.
+        """
+        super().__init__(
+            graph,
+            loader_id,
+            num_batches,
+            buffer_size,
+            output_format,
+            kafka_address,
+            kafka_max_msg_size,
+            kafka_num_partitions,
+            kafka_replica_factor,
+            kafka_retention_ms,
+            kafka_auto_del_topic,
+            kafka_address_consumer,
+            kafka_address_producer,
+            timeout,
+        )
+        # Resolve attributes
+        self.v_in_feats = self._validate_vertex_attributes(v_in_feats)
+        self.v_out_labels = self._validate_vertex_attributes(v_out_labels)
+        self.v_extra_feats = self._validate_vertex_attributes(v_extra_feats)
+        # Initialize parameters for the query
+        self._payload = {}
+        if batch_size:
+            # If batch_size is given, calculate the number of batches
+            num_vertices_by_type = self._graph.getVertexCount("*")
+            if filter_by:
+                num_vertices = sum(
+                    self._graph.getVertexCount(k, where="{}!=0".format(filter_by))
+                    for k in num_vertices_by_type
+                )
+            else:
+                num_vertices = sum(num_vertices_by_type.values())
+            self.num_batches = math.ceil(num_vertices / batch_size)
+        else:
+            # Otherwise, take the number of batches as is.
+            self.num_batches = num_batches
+        self._payload["num_batches"] = self.num_batches
+        self._payload["num_neighbors"] = num_neighbors
+        self._payload["num_hops"] = num_hops
+        if filter_by:
+            self._payload["filter_by"] = filter_by
+        self._payload["shuffle"] = shuffle
+        if self.kafka_address_producer:
+            self._payload["kafka_address"] = self.kafka_address_producer
+        # kafka_topic will be filled in later.
+        # Output
+        self.add_self_loop = add_self_loop
+        # Install query
+        self.query_name = self._install_query()
+
+    def _install_query(self):
+        # Install the right GSQL query for the loader.
+        v_attr_names = self.v_in_feats + self.v_out_labels + self.v_extra_feats
+        query_replace = {"{QUERYSUFFIX}": "_".join(v_attr_names)}
+        attr_types = next(iter(self._v_schema.values()))
+        if v_attr_names:
+            query_print = '+","+'.join(
+                "{}(s.{})".format(_udf_funcs[attr_types[attr]], attr)
+                for attr in v_attr_names
+            )
+            query_replace["{VERTEXATTRS}"] = query_print
+        else:
+            query_replace['+ "," + {VERTEXATTRS}'] = ""
+        query_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "gsql",
+            "dataloaders",
+            "neighbor_loader.gsql",
+        )
+        return self._install_query_file(query_path, query_replace)
+
+    def _start(self) -> None:
+        # Create task and result queues
+        self._read_task_q = Queue(self.buffer_size * 2)
+        self._data_q = Queue(self.buffer_size)
+        self._exit_event = Event()
+
+        # Start requesting thread.
+        if self.kafka_address_consumer:
+            # If using kafka
+            self._kafka_topic = "{}_{}".format(self.loader_id, self._iterations)
+            self._payload["kafka_topic"] = self._kafka_topic
+            self._requester = Thread(
+                target=self._request_kafka,
+                args=(
+                    self._exit_event,
+                    self._graph,
+                    self.query_name,
+                    self._kafka_consumer,
+                    self._kafka_admin,
+                    self._kafka_topic,
+                    self.kafka_partitions,
+                    self.kafka_replica,
+                    self.max_kafka_msg_size,
+                    self.kafka_retention_ms,
+                    self.timeout,
+                    self._payload,
+                ),
+            )
+        else:
+            # Otherwise, use rest api
+            self._requester = Thread(
+                target=self._request_rest,
+                args=(
+                    self._graph,
+                    self.query_name,
+                    self._read_task_q,
+                    self.timeout,
+                    self._payload,
+                    "both",
+                ),
+            )
+        self._requester.start()
+
+        # If using Kafka, start downloading thread.
+        if self.kafka_address_consumer:
+            self._downloader = Thread(
+                target=self._download_from_kafka,
+                args=(
+                    self._exit_event,
+                    self._read_task_q,
+                    self.num_batches,
+                    True,
+                    self._kafka_consumer,
+                ),
+            )
+            self._downloader.start()
+
+        # Start reading thread.
+        v_attr_types = next(iter(self._v_schema.values()))
+        v_attr_types["is_seed"] = "bool"
+        if self.kafka_address_consumer:
+            raw_format = "graph_bytes"
+        else:
+            raw_format = "graph_str"
+        self._reader = Thread(
+            target=self._read_data,
+            args=(
+                self._exit_event,
+                self._read_task_q,
+                self._data_q,
+                raw_format,
+                self.output_format,
+                self.v_in_feats,
+                self.v_out_labels,
+                self.v_extra_feats + ["is_seed"],
+                v_attr_types,
+                [],
+                [],
+                [],
+                {},
+                self.add_self_loop,
+                True,
+            ),
+        )
+        self._reader.start()
+
